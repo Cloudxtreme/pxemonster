d18d2350beb3c30b1b1b037f561ae54e0b2def8d
cbitte000 <charles_bitter@cable.comcast.com>
2016-04-13 09:37:58 -0400

    removing puts

--- lib/pxe_linux.rb
+++ lib/pxe_linux.rb
@@ -14,8 +14,6 @@ class PXELinux
 
 	def get(ip)
 		info = @host_configs[ip]
-		puts "get: the host confis is #{@host_configs}"
-		puts "get: info = #{info}"
 		raise NoConfigFoundForIp.new(ip) if info.nil?
 		info
 	end
@@ -31,8 +29,6 @@ class PXELinux
 	def delete(ip)
 	
 		info = @host_configs[ip.to_s]
-		puts "delete: the host confis is #{@host_configs}"
-		puts "delete: info = #{info}" 
 		raise NoConfigFoundForIp.new(ip) if info.nil?
 		pxe_file = @host_configs.config_dir.join(info["pxe_file_name"])
 		pxe_file.delete unless pxe_file.exist?
